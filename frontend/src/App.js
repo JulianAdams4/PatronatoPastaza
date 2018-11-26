b3353a728dabaafdb0f563d4e48bec68c5246b5c
Julian Adams <jadams@espol.edu.ec>
2018-11-26 06:09:51 -0500

    Login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import Home from './components/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginWrapper from './components/LoginWrapper';
 
-const App = () => <Home />;
+const Dash = () => (
+  <h3>Dashborad</h3>
+);
+
+const App = () => (
+  <Router>
+    <div className="app">
+      <Route path="/" component={LoginWrapper} />
+      <Route exact path="/home" component={Dash} />
+    </div>
+  </Router>
+);
 
 export default App;
