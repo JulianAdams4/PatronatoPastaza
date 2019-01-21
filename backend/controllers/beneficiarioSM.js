212c42b4e179bd3e4eb9d2e0ad21823c8ec7f754
erialper <erialper@espol.edu.ec>
2019-01-21 18:53:16 -0500

    Api beneficiario 7.5

--- backend/controllers/beneficiarioSM.js
+++ backend/controllers/beneficiarioSM.js
@@ -1,5 +1,89 @@
 const db = require('../database')
 
+
+/*
+if(req.body.nombre != '' && req.body.apellido != '' && req.body.edad != '' &&
+   req.body.nacionalidad != '' && req.body.grupoCultural != '' && req.body.sexo != '' &&
+   req.body.zona != '' && req.body.estadoCivil != '' && req.body.resNombre != '' &&
+   req.body.resApellido != '' && req.body.resParentesco != ''){
+
+*/
+
+const ingresarBeneficiarioSM = (req, res, next) => {
+  console.log(req.body.nombre);
+  console.log(req.body.apellido);
+  if(req.body.nombre!="" && req.body.apellido!=""){
+    console.log(req.body.apellido);
+    res.status(200).json({
+      error: false
+    });
+  }else{
+    console.log(req.body.nombre);
+    res.status(500).json({
+      error: true,
+      data:{
+        message:'Información Faltante'
+      }
+    });
+  }
+   /*
+    db('beneficiario').insert({
+      nombre: req.body.nombre,
+      apellido: req.body.apellido,
+      identificacion: req.body.identificacion,
+      direccion: req.body.direccion,
+      barrio: req.body.barrio,
+      zona: req.body.zona,
+      telefono: req.body.telefono,
+      fechaNacimiento: req.body.fechaNacimiento,
+      edad: req.body.edad,
+      lugarNacimiento: req.body.lugarNacimiento,
+      nacionalidad: req.body.nacionalidad,
+      grupoCultural: req.body.grupoCultural,
+      sexo: req.body.sexo,
+      estadoCivil: req.body.estadoCivil,
+      instruccion: req.body.instruccion,
+      empresa: req.body.empresa,
+      ocupacion: req.body.ocupacion,
+      seguro: req.body.seguro,
+      referido: req.body.referido,
+      id_parroquia: req.body.parroquia
+    })
+    .then(function(id){
+      db('responsable').insert({
+        nombre: req.body.resNombre,
+        apellido: req.body.resApellido,
+        parentesco: req.body.resParentesco,
+        telefono: req.body.resTelefono,
+        direccion: req.body.resDireccion,
+        id_beneficiario: id,
+      })
+      .then(function(id){
+        res.json({
+          error: false,
+          data: id
+        })
+      })
+      .catch(function(err){
+        res.status(500).json({
+          error: true,
+          data:{
+            message:err.message
+          }
+        })
+      })
+    })
+    .catch(function(err){
+      res.status(500).json({
+        error: true,
+        data:{
+          message:err.message
+        }
+      })
+    })
+    */
+};
+
 const consultarBeneficiarioSM = (req, res, next) => {
   db.select('beneficiario.id','nombre','apellido','identificacion','telefono').from('beneficiario')
   .join('admision','beneficiario.id','admision.id_beneficiario')
@@ -51,7 +135,97 @@ const filtrarBeneficiarioSM = (req, res, next) => {
   })
 };
 
+const filtrarBeneficiario = (req, res, next) => {
+  db.select('beneficiario.id','nombre','apellido','identificacion','telefono').from('beneficiario')
+  .where((qb) => {
+    if (req.body.nombre!="") {
+      qb.where('nombre', 'like', '%'+req.body.nombre+'%');
+    }
+    if (req.body.apellido!="") {
+      qb.orWhere('apellido', 'like', '%'+req.body.apellido+'%');
+    }
+    if (req.body.identificacion!="") {
+      qb.orWhere('identificacion', 'like', '%'+req.body.identificacion+'%');
+    }
+  })
+  .then(function(collection){
+    res.json({
+      error: false,
+      data: collection
+    })
+  })
+  .catch(function(err){
+    res.status(500).json({
+      error: true,
+      data:{
+        message:err.message
+      }
+    })
+  })
+};
+
+const consultarProvincia = (req, res, next) => {
+  db.select('nombre').from('provincia')
+  .then(function(collection){
+    res.json({
+      error: false,
+      data: collection
+    })
+  })
+  .catch(function(err){
+    res.status(500).json({
+      error: true,
+      data:{
+        message:err.message
+      }
+    })
+  })
+};
+
+const consultarCanton = (req, res, next) => {
+  db.select('nombre').from('canton')
+  .where('id_provincia',req.params.id)
+  .then(function(collection){
+    res.json({
+      error: false,
+      data: collection
+    })
+  })
+  .catch(function(err){
+    res.status(500).json({
+      error: true,
+      data:{
+        message:err.message
+      }
+    })
+  })
+};
+
+const consultarParroquia = (req, res, next) => {
+  db.select('nombre').from('parroquia')
+  .where('id_canton',req.params.id)
+  .then(function(collection){
+    res.json({
+      error: false,
+      data: collection
+    })
+  })
+  .catch(function(err){
+    res.status(500).json({
+      error: true,
+      data:{
+        message:err.message
+      }
+    })
+  })
+};
+
 module.exports = {
+  ingresarBeneficiarioSM,
   consultarBeneficiarioSM,
-  filtrarBeneficiarioSM
+  filtrarBeneficiarioSM,
+  filtrarBeneficiario,
+  consultarProvincia,
+  consultarCanton,
+  consultarParroquia
 };
--- backend/routes/beneficiarioSM.js
+++ backend/routes/beneficiarioSM.js
@@ -2,9 +2,19 @@ const controlador = require('../controllers/beneficiarioSM')
 const express = require('express');
 const router = express.Router();
 
+router.post('/', controlador.ingresarBeneficiarioSM);
+
 router.post('/filtrar', controlador.filtrarBeneficiarioSM);
+
+router.post('/todos', controlador.filtrarBeneficiario);
 
 router.get('/', controlador.consultarBeneficiarioSM);
 //router.post('/',);
+
+router.get('/provincia', controlador.consultarProvincia);
+
+router.get('/canton/:id', controlador.consultarCanton);
+
+router.get('/parroquia/:id', controlador.consultarParroquia);
 
 module.exports = router;
